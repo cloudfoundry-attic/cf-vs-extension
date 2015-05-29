51736e07d1f5df6f0beda4780572bd429fa43645
Daniel Carabas <daniel.carabas@hp.com>
2015-05-29 16:55:24 +0300

    Add create service dialog

--- src/CloudFoundry.VisualStudio/Controls/ServicesUserControl.xaml.cs
+++ src/CloudFoundry.VisualStudio/Controls/ServicesUserControl.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CloudFoundry.VisualStudio.Forms;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,11 @@ namespace CloudFoundry.VisualStudio.Controls
         public ServicesUserControl()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
         }
     }
 }
