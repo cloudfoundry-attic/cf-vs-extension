111559531780aedad438e05379e5596310e7bdd4
Daniel Carabas <daniel.carabas@hp.com>
2015-06-03 12:23:49 +0300

    Fix environment variables user control

--- src/CloudFoundry.VisualStudio/Controls/EnvVarsUserControl.xaml.cs
+++ src/CloudFoundry.VisualStudio/Controls/EnvVarsUserControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using CloudFoundry.VisualStudio.Forms;
+using CloudFoundry.VisualStudio.ProjectPush;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,45 @@ namespace CloudFoundry.VisualStudio.Controls
         public EnvVarsUserControl()
         {
             InitializeComponent();
+        }
+
+        private void btnAddEnvVar_Click(object sender, RoutedEventArgs e)
+        {  
+            var profile = (this.DataContext as PublishProfile);
+
+            if (!profile.CFAppManifest.EnvironmentVars.ContainsKey(tbEnvVarKey.Text))
+            {
+                profile.CFAppManifest.EnvironmentVars.Add(tbEnvVarKey.Text, tbEnvVarValue.Text);
+
+                tbEnvVarValue.Clear();
+                tbEnvVarKey.Clear();
+                dgEnvVars.Items.Refresh();
+            }
+            else
+            {
+                MessageBoxHelper.DisplayError("An environment variable with the same key has already been added!");
+            }
+        }
+
+        private void Delete_Click(object sender, RoutedEventArgs e)
+        {
+            if (dgEnvVars.SelectedItem != null)
+            {
+                try
+                {
+                    KeyValuePair<string, string> selectedEnvVar = (KeyValuePair<string, string>)dgEnvVars.SelectedItem;
+                    var profile = (this.DataContext as PublishProfile);
+                    if (profile != null)
+                    {
+                        profile.CFAppManifest.EnvironmentVars.Remove(selectedEnvVar.Key);
+                    }
+                    dgEnvVars.Items.Refresh();
+                }
+                catch (Exception ex)
+                {
+                    MessageBoxHelper.DisplayError(ex);
+                }
+            }
         }
     }
 }
