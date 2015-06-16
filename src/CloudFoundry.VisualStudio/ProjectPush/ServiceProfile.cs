f28649735ae190bb8f3aec4bfb4e49bfe15266e3
Mihai Buzgau <mihai.buzgau@hp.com>
2015-06-16 17:23:59 +0300

    fix for ui on services, env vars and routes

---
+++ src/CloudFoundry.VisualStudio/ProjectPush/ServiceProfile.cs
@@ -0,0 +1,20 @@
+﻿using CloudFoundry.CloudController.V2.Client.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloudFoundry.VisualStudio.ProjectPush
+{
+    public class ServiceProfile
+    {
+        public ListAllServiceInstancesForSpaceResponse ServiceInstance { get; set; }
+
+        public RetrieveServicePlanResponse ServicePlan { get; set; }
+
+        public RetrieveServiceResponse Service { get; set; }
+
+        public bool Used { get; set; }
+    }
+}
